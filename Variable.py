f31e482c7c63eb4863987b1e9767e30c36b090ec
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-06 15:34:15 -0700

    clean up

---
+++ Variable.py
@@ -0,0 +1,22 @@
+bill = float(input('How much is the bill? '))
+tip = int(input('What percentage is the tip?'))
+total = (((bill*tip)/100) + (bill))
+print ('The total is: ' + str(total))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
