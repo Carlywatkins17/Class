1a46ee9aac9e1b182ae8cd1078e3f25bb9d7673c
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-10 15:49:05 -0700

    added documentation

---
+++ Userinput.py
@@ -0,0 +1,21 @@
+A = 10
+B = 20
+c = 30
+D = 2.5
+E = 3.5
+answer = input("Type the sum of 10+20")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Variable.py
+++ Variable.py
@@ -3,12 +3,40 @@
 # CS-100A-x
 # License: Copyright 2022 Carly Watkins
 # Resources used: https://www.google.com/search?q=google+style+guide&rlz=1C1GCEJ_enUS1024US1024&oq=google+style&aqs=chrome.1.69i57j0i131i433i512j0i512l5j69i60.3639j1j7&sourceid=chrome&ie=UTF-8
-#Tip calculator exercise from night 1
+#Variable exercise from night 1
+
+from audioop import avg
+
 
 bill = float(input('How much is the bill? '))
 tip = int(input('What percentage is the tip?'))
 total = (((bill*tip)/100) + (bill))
 print ('The total is: ' + str(total))
+
+A = 10
+B = 20
+C = 30
+D = 10.5
+E = 20.5
+F = 30.5
+
+print (A+B)
+print (F-C)
+print (D*E)
+print (C/B)
+print(F%E)
+
+game1 = 13
+game2 = 18
+game3 = 21
+game4 = 17
+game5 = 31
+
+print((game1 + game2 + game3 + game4 + game5)/5)
+
+
+
+
 
 
 
