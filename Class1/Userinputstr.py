bde00044dc8406ca37b7d176ee865f38de0a4c1d
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-10 18:21:56 -0700

    created new folder structure

---
+++ Class1/Userinputstr.py
@@ -0,0 +1,20 @@
+# Carly Watkins
+# 10/6
+# CS-100A-x
+# License: Copyright 2022 Carly Watkins
+# Resources used: https://www.google.com/search?q=google+style+guide&rlz=1C1GCEJ_enUS1024US1024&oq=google+style&aqs=chrome.1.69i57j0i131i433i512j0i512l5j69i60.3639j1j7&sourceid=chrome&ie=UTF-8
+# Variable exercises from night strings
+
+# Exercise 8
+
+username = input("What is your name? ")
+print("Welcome to the program " + username + "!")
+
+# Exercise 9
+
+adj = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+ptv = input("Enter a past tense verb: ")
+noun2 = input("Enter another noun: ")
+
+print("The quick " + )
