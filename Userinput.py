525c7769663e41aeb5ca7a6c4fa2a5ef1498525d
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-10 17:42:23 -0700

    finished exercise 4

--- Userinput.py
+++ Userinput.py
@@ -1,9 +1,48 @@
-A = 10
-B = 20
-c = 30
-D = 2.5
-E = 3.5
-answer = input("Type the sum of 10+20")
+A = int(input("Choose a an interger to represent A: "))
+B = int(input("Enter an interger for B: "))
+C = int(input("Enter an interger number for C: "))
+
+D = float(input("Choose a real number for D: "))
+E = float(input("Choose a real number for E: "))
+F = float(input("Choose a real number for F: "))
+
+Q1 = int(input("What is A + B? "))
+if Q1 == (A + B):
+    print("Thats right!")
+elif Q1 != (A + B):
+    print("The correct answer is " + str(A + B))
+    
+
+Q2 = int(input("What is B - C? "))
+if Q2 == (B - C):
+    print("Thats right!")
+elif Q2 != (B - C):
+    print("The correct answer is " + str(B - C))
+    
+
+Q3 = int(input("What is C * D? "))
+if Q3 == (C * D):
+    print("Thats right!")
+elif Q3 != (C * D):
+    print("The correct answer is " + str(C * D))
+    
+
+Q4 = int(input("What is D / E? "))
+if Q4 == (D / E):
+    print("Thats right!")
+elif Q4 != (D / E):
+    print("The correct answer is " + str(D / E))
+    
+
+Q5 = int(input("What is the remainder of E / F? "))
+if Q5 == (E % F):
+    print("Thats right!")
+elif Q5 != (E % F):
+    print("The correct answer is " + str(E % F))
+    
+
+
+
 
 
 
