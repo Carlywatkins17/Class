f7acc1e9508e13f96e16ad3f700d392e60c6521e
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-10 21:55:26 -0700

    finished wrap up

--- session1/Wrapup.py
+++ session1/Wrapup.py
@@ -16,10 +16,29 @@ print("E = " + str(F))
 
 
 Q1 = int(input("What is A + B ? "))
-if Q1 == A + B:
-    print('Thats right!')
-elif Q1 != A + B:
-    print('The correct answer is ' + str(A + B))
+Q2 = int(input("What is A - B ? "))
+Q3 = int(input("What is C * D ? "))
+Q4 = int(input("What is E / F ? "))
 
+
+if Q1 == A + B:
+    print('Queston 1: correct')
+elif Q1 != A + B:
+    print('Question 1 incorrect. The correct answer is ' + str(A + B))
+
+    if Q2 == A - B:
+    print('Queston 2: correct')
+elif Q2 != A - B:
+    print('Question 2 incorrect. The correct answer is ' + str(A - B))
+
+    if Q3 == C * D:
+    print('Queston 3: correct')
+elif Q3 != C * D:
+    print('Question 3 incorrect. The correct answer is ' + str(C * D))
+
+    if Q4 == E / F:
+    print('Queston 4: correct')
+elif Q4 != E / F:
+    print('Question 4 incorrect. The correct answer is ' + str(E / F))
 
 
