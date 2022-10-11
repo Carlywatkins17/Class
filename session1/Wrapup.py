6669afeb018d89ed70094f0534ed1edbbc95d3e4
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-10 21:56:31 -0700

    correction to wrap up

--- session1/Wrapup.py
+++ session1/Wrapup.py
@@ -26,17 +26,17 @@ if Q1 == A + B:
 elif Q1 != A + B:
     print('Question 1 incorrect. The correct answer is ' + str(A + B))
 
-    if Q2 == A - B:
+if Q2 == A - B:
     print('Queston 2: correct')
 elif Q2 != A - B:
     print('Question 2 incorrect. The correct answer is ' + str(A - B))
 
-    if Q3 == C * D:
+if Q3 == C * D:
     print('Queston 3: correct')
 elif Q3 != C * D:
     print('Question 3 incorrect. The correct answer is ' + str(C * D))
 
-    if Q4 == E / F:
+if Q4 == E / F:
     print('Queston 4: correct')
 elif Q4 != E / F:
     print('Question 4 incorrect. The correct answer is ' + str(E / F))
