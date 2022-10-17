f5dda7099959d8f2df65449d6c3cac5aaa6a99b9
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-17 16:22:50 -0700

    loop2

---
+++ Session2/Loop.py
@@ -0,0 +1,62 @@
+# Carly Watkins
+# 10/13
+# CS-100A-x
+# License: Copyright 2022 Carly Watkins
+# Resources used: https://www.google.com/search?q=google+style+guide&rlz=1C1GCEJ_enUS1024US1024&oq=google+style&aqs=chrome.1.69i57j0i131i433i512j0i512l5j69i60.3639j1j7&sourceid=chrome&ie=UTF-8
+# exercises from night 2
+
+#Exercise 4 go back to rock paper scissors.  make it loop until someone wins 
+
+from platform import machine
+import random
+
+Rock = 1 
+Paper = 2 
+Scissors = 3
+
+
+User = int(input("Enter your choice: "))
+
+Machine = (random.randrange(1,3))
+
+print ("Rock is one, Paper is 2, Scissors is 3")
+
+print("The machine chooses " + str(Machine))
+
+if User == 1 and Machine == 3:
+    print('You win!')
+if User == 1 and Machine == 2:
+    print('The machine wins!')
+if User == 2 and Machine == 1:
+    print('You win!')
+if User == 2 and Machine == 3:
+    print('The machine wins!')
+if User == 3 and Machine == 2: 
+    print('You win!')
+if User == 3 and Machine == 1:
+    print('The machine wins!')
+if User == Machine:
+    print('Its a tie.  Pick again')
+    User = int(input("Enter your choice: "))
+
+    Machine = (random.randrange(1,3))
+
+    print("The machine chooses " + str(Machine))
+
+    if User == 1 and Machine == 3:
+        print('You win!')
+    if User == 1 and Machine == 2:
+        print('The machine wins!')
+    if User == 2 and Machine == 1:
+        print('You win!')
+    if User == 2 and Machine == 3:
+        print('The machine wins!')
+    if User == 3 and Machine == 2: 
+        print('You win!')
+    if User == 3 and Machine == 1:
+        print('The machine wins!')
+    if User == Machine:
+        print('Its a tie.  Pick again')
+
+
+
--- Session2/Strings.py
+++ Session2/Strings.py
@@ -3,7 +3,7 @@
 # CS-100A-x
 # License: Copyright 2022 Carly Watkins
 # Resources used: https://www.google.com/search?q=google+style+guide&rlz=1C1GCEJ_enUS1024US1024&oq=google+style&aqs=chrome.1.69i57j0i131i433i512j0i512l5j69i60.3639j1j7&sourceid=chrome&ie=UTF-8
-# conditional exercises from night 1
+# exercises from night 2
 
 # Strings Exercises
 
