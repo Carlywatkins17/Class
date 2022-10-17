a720152348a1ef304128b041fdff6dd603488579
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-17 14:41:52 -0700

    updated exercises

--- Session2/Exercises.py
+++ Session2/Exercises.py
@@ -9,10 +9,44 @@
 
 #Exercise 1 Write a program that asks the user for a string and then outputs that string in various forms: upper case, lower case, and "title case".
 
+from base64 import encode
+from gettext import find
+from itertools import count
+from operator import index
 from tkinter import CENTER
 
-text = ('Give me a string: ')
-print(text)
+
+text = (input('Give me a string: '))
+print(text.upper())
+print(text.lower())
+print(text.istitle())
+
+#Exercise 2 Write origram asks the user for alink to a website.  Use the startswith to check that the link starts with the string http.  
+# If it does, thank the user for giving a valid link
+#If not, tell the user they gave an invalid link.  
+
+Thanks = True
+link = input('Please enter the URL of your favorite weather website: ')
+if (link.startswith('http') True):
+    print('Thank you')
+elif (link.startswith('http') False):
+    print('That is an invalid link')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
