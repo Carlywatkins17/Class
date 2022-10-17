66a9da4c4befe9c00ea19a0ec53e831ceb46d68c
Carly Watkins <Carly.Watkins@pemco.com>
2022-10-17 13:47:10 -0700

    started session 2

---
+++ Session2/Exercises.py
@@ -0,0 +1,19 @@
+# Carly Watkins
+# 10/13
+# CS-100A-x
+# License: Copyright 2022 Carly Watkins
+# Resources used: https://www.google.com/search?q=google+style+guide&rlz=1C1GCEJ_enUS1024US1024&oq=google+style&aqs=chrome.1.69i57j0i131i433i512j0i512l5j69i60.3639j1j7&sourceid=chrome&ie=UTF-8
+# conditional exercises from night 1
+
+# Strings Exercises
+
+#Exercise 1 Write a program that asks the user for a string and then outputs that string in various forms: upper case, lower case, and "title case".
+
+from tkinter import CENTER
+
+text = ('Give me a string: ')
+print(text)
+
+
+
+
--- session1/Wrapup.py
+++ session1/Wrapup.py
@@ -1,3 +1,11 @@
+# Carly Watkins
+# 10/6
+# CS-100A-x
+# License: Copyright 2022 Carly Watkins
+# Resources used: https://www.google.com/search?q=google+style+guide&rlz=1C1GCEJ_enUS1024US1024&oq=google+style&aqs=chrome.1.69i57j0i131i433i512j0i512l5j69i60.3639j1j7&sourceid=chrome&ie=UTF-8
+# conditional exercises from night 1
+
+
 username = input('What is your name? ')
 
 import random
